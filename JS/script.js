2d27b4c02c7a08a6111372e57b53d7df81eaefff
Kim <lindholmskim90@gmail.com>
2019-10-26 13:16:44 +0300

    Comments and change in CSS file
    Added some comments and forced my footer tag to be at the bottom of the page, next step to design a well new task input

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+//Document have to be "ready" so all the functionality will work when the event start to happend
 $(document).ready(function(){
   $("button").click(function(){
     $("#NewTask").show();
