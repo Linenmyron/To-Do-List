4771b3277a5cf7829a5558916b450b32936f28c8
Kim <lindholmskim90@gmail.com>
2019-10-26 14:35:22 +0300

    Removed img directory and adding more functionality to the buttons
    added ok and cancel button to new task div

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,20 @@
 //Document have to be "ready" so all the functionality will work when the event start to happend
 $(document).ready(function(){
-  $("button").click(function(){
+  //This function will show the NewTask div
+  $("#CreateTask").click(function(){
     $("#NewTask").show();
   });
+
+  //This Cancel button function will cancel and hide the NewTask div
+  $("#cancel").click(function(){
+    $("#NewTask").hide();
+  });
+
+  //This create button will add a div to first article when pressed and hide NewTask div
+  $()"#ok").click(function(){
+    
+  });
+
+
+//End of the document ready function
 });
